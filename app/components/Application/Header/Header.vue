8fea9507075afe76f34432ec9814b76f583e9f79
Tom <tom.van.veen@visma.com>
2025-03-27 17:07:05 +0100

    style: colors

--- app/components/Application/Header/Header.vue
+++ app/components/Application/Header/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="z-10 shadow-xl shadow-neutral-500/10 fixed flex items-center left-0 right-0 top-0 bg-neutral-100 dark:bg-neutral-900 border-b border-b-neutral-100 dark:border-b-neutral-800 h-[var(--application-header-height)]">
+  <header class="z-10 shadow-xl shadow-neutral-500/10 dark:shadow-neutral-950/50 fixed flex items-center left-0 right-0 top-0 bg-neutral-100 dark:bg-neutral-900 border-b border-b-neutral-100 dark:border-b-neutral-800 h-[var(--application-header-height)]">
     <UContainer class="flex items-center h-20 w-full">
       <nav>
         <ul class="flex gap-2">
